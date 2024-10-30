497723cc4b358ce308f56695caafafed25ed1f8a
Shridhar1605 <shridharmohanraj@gmail.com>
2024-10-30 16:59:07 +0530

    Create First_Missing_positive.cpp

---
+++ First_Missing_positive.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int check = 1;
+        for (int i = 0; i < nums.size(); i++){
+            if (nums[i] <= 0)
+                continue;
+
+            if (nums[i] == check) check++;
+
+            if (nums[i] > check) return check;
+     
+        }
+        return check;
+    }
+    
+};
+//Don't why it's marked hard
